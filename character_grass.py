47a863f0ef1e77668d353ce3ee64aab7f9f43b8c
subinlee@notebook <lsbin0219@gmail.com>
2024-09-24 12:27:48 +0900

    사각운동 기본 흐름구조 완성(함수로)

--- character_grass.py
+++ character_grass.py
@@ -10,7 +10,7 @@ def run_circle():
     print('CIRCLE')
 
     r, cx, cy = 300, 800 // 2, 600 // 2
-    #400을 쓰지 않는 이유는 화면 크기의 반이다는 것을 보여주기 위함
+    # 400을 쓰지 않는 이유는 화면 크기의 반이다는 것을 보여주기 위함
     
     for degree in range(0, 360, 3):
         theta = math.radians(degree)
@@ -20,15 +20,39 @@ def run_circle():
         character.draw_now(x,y)
         delay(0.1)
     
-    pass    #pass는 빈내용을 뜻함
+    # pass    # pass는 빈내용을 뜻함
+
+def run_top():
+    print('TOP')
+    pass
+
+def run_right():
+    print('RIGHT')
+    pass
+
+def run_bottom():
+    print('BOTTOM')
+    pass
+
+def run_left():
+    print('LEFT')
+    pass
+
 
 def run_rectangle():
     print('RECTANGLE')
+
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
+    
     pass
 
 while True:
+    # run_circle()
     run_rectangle()
-    run_circle()
+    
     break
 
     
