00e17280bccc8753118d186b5f5dbb85bf2ffa4a
subinlee@notebook <lsbin0219@gmail.com>
2024-09-24 12:12:43 +0900

    원 운동 함수 구현 완료

--- character_grass.py
+++ character_grass.py
@@ -9,9 +9,16 @@ character = load_image('character.png')
 def run_circle():
     print('CIRCLE')
 
-    clear_canvas_now()
-    character.draw_now(400,300)
-    delay(0.1)
+    r, cx, cy = 300, 800 // 2, 600 // 2
+    #400을 쓰지 않는 이유는 화면 크기의 반이다는 것을 보여주기 위함
+    
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+        clear_canvas_now()
+        character.draw_now(x,y)
+        delay(0.1)
     
     pass    #pass는 빈내용을 뜻함
 
